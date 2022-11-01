bfc27ae3a0928cb33055c6584883996034fb9675
terranceliu <terranceliu.tl64@gmail.com>
2022-11-01 01:23:50 -0400

    update ppmf scripts

--- examples/data_preprocessing/scripts/preprocess_blocks.sh
+++ examples/data_preprocessing/scripts/preprocess_blocks.sh
@@ -5,5 +5,6 @@ ALL_FIPS=(01 02 04 05 06 08 09 10 12 13 15 16 17 18 19 20 21 22 23 24 25 26 27 2
 
 for STATE_ID in "${ALL_FIPS[@]}"
 do
+  echo $STATE_ID
   python examples/data_preprocessing/preprocess_ppmf_blocks.py --stateid $STATE_ID
 done
--- examples/data_preprocessing/scripts/preprocess_tracts.sh
+++ examples/data_preprocessing/scripts/preprocess_tracts.sh
@@ -3,11 +3,10 @@
 # DC: 11
 ALL_FIPS=(01 02 04 05 06 08 09 10 12 13 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42 44 45 46 47 48 49 50 51 53 54 55 56 11)
 
-for SEED in 0 1 2 3 4
+SEED=0
+
+for STATE_ID in "${ALL_FIPS[@]}"
 do
-  echo $SEED
-  for STATE_ID in "${ALL_FIPS[@]}"
-  do
-    python examples/data_preprocessing/preprocess_ppmf.py --stateid $STATE_ID --seed $SEED
-  done
+  echo $STATE_ID
+  python examples/data_preprocessing/preprocess_ppmf.py --stateid $STATE_ID --seed $SEED
 done
